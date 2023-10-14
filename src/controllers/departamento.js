439d8ed6615f87b9736620aaf6cc439f27cff010
EminatarenX <eminataren2002@gmail.com>
2023-10-14 14:57:28 -0600

    anadiendo mensaje de error al obtener departamentos

--- src/controllers/departamento.js
+++ src/controllers/departamento.js
@@ -12,8 +12,8 @@ const obtenerDepartamentos = async (req, res) => {
 
         return res.status(200).json({ departamentos})
     }catch(error){
-        console.log(error)
-        return res.status(500).json({ error })
+        
+        return res.status(500).json({msg: 'Hubo un error', error })
     }
 }
 
