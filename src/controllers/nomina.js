f0dc1ff4123f5c143d87add4ac570c08a9f75254
Eminataren <eminataren2002@gmail.com>
2023-11-26 22:21:30 -0600

    mejorando nomina

--- src/controllers/nomina.js
+++ src/controllers/nomina.js
@@ -1,176 +1,205 @@
-import { 
-    calcularCuotasObrero,
-    calcularFactorIntegracion,
-    calcularISR ,
-    calcularSubsidio
-} from "../helpers/quincenal.js"
-import Nomina from "../models/Nomina.js"
-import Empleado from '../models/Empleado.js'
-import Check from '../models/Check.js'
-import { generarNominaPdf } from "../helpers/pdfs/generarNominaPdf.cjs"
-import { uploadFile, getFile, getAllNominas, deleteDocument } from "../helpers/clientAws.js"
-import { enviarNominaTrabajador } from '../helpers/correos.js'
+import {
+  calcularCuotasObrero,
+  calcularFactorIntegracion,
+  calcularISR,
+  calcularSubsidio,
+} from "../helpers/quincenal.js";
+import Nomina from "../models/Nomina.js";
+import Empleado from "../models/Empleado.js";
+import Check from "../models/Check.js";
+import { generarNominaPdf } from "../helpers/pdfs/generarNominaPdf.cjs";
+import {
+  uploadFile,
+  getFile,
+  getAllNominas,
+  deleteDocument,
+} from "../helpers/clientAws.js";
+import { enviarNominaTrabajador } from "../helpers/correos.js";
 
-const generarNomina = async(req, res) => {
-    const { empleado: empleado_id } = req.body
+const generarNomina = async (req, res) => {
+  const { empleado: empleado_id } = req.body;
 
-    try {
-        const empleado = await Empleado.findById(empleado_id)
-        .populate("usuario")
-        .populate("empresa")
-        .populate("plaza")
-    
-        if(!empleado) return res.status(404).json({ mensaje: 'Empleado no encontrado' })
+  try {
+    const empleado = await Empleado.findById(empleado_id)
+      .populate("usuario")
+      .populate("empresa")
+      .populate("plaza");
 
-        const dias_vacaciones = empleado.dias_vacaciones
-        const dias_aginaldo = empleado.dias_aguinaldo
-        
-    
-        const { usuario, empresa, plaza } = empleado
-    
-        const quinceDiasAtras = new Date();
-        quinceDiasAtras.setDate(quinceDiasAtras.getDate() - 15);
-    
-        const checks = await Check.find({ empleado: empleado_id, fecha_entrada: { $gte: quinceDiasAtras }})
+    if (!empleado)
+      return res.status(404).json({ mensaje: "Empleado no encontrado" });
 
-        
-        // verificar si aun no se ha hecho una nomina con la misma fecha de inicio y fin
+    const dias_vacaciones = empleado.dias_vacaciones;
+    const dias_aginaldo = empleado.dias_aguinaldo;
 
-        let dias_laborados = checks.length
+    const { usuario, empresa, plaza } = empleado;
 
-        if(dias_laborados === 0) return res.status(404).json({ mensaje: 'No hay registros de dias laborados en los últimos 15 días' })
-        
-        let salario_diario = Number(plaza.salario)
-        
-       
-    
-        const factor_integracion = calcularFactorIntegracion(dias_vacaciones, dias_aginaldo)
-        const salario_diario_integrado = salario_diario <= 207.44 ? 5255.5 / 15 : salario_diario * factor_integracion
-        let cuotas_obrero = calcularCuotasObrero(salario_diario_integrado, dias_laborados)
-        let ISR = calcularISR(salario_diario, dias_laborados)
-      
-        if(salario_diario <= 207.44) {
-            ISR = 0
-            cuotas_obrero = 0
-        }   
-       
-        
-        const suma_deducciones = cuotas_obrero + ISR
-        const salario_bruto = salario_diario * dias_laborados
-    
-        const nomina = await Nomina.create({
-            empleado: empleado_id,
-            empresa: empresa._id,
-            plaza: plaza._id,
-            percepciones: {
-                dias_laborados,
-                fecha_inicio: checks[0].fecha_entrada,
-                fecha_fin: checks[checks.length - 1].fecha_entrada,
-                sueldo: salario_bruto,
-                salario_diario,
-                salario_diario_integrado,
-                subsidio: calcularSubsidio(salario_diario, dias_laborados),
-                neto: salario_bruto - suma_deducciones,
-            },
-            deducciones: {
-                ISR,
-                cuotas_obrero,
-            }
-        })
+    const quinceDiasAtras = new Date();
+    quinceDiasAtras.setDate(quinceDiasAtras.getDate() - 15);
 
-        const pdf = await generarNominaPdf({nomina, empleado, empresa, plaza, usuario})
-        const emision = nomina.fecha_emision.toString().split('GMT')[0].trim().replace(/ /g, '-')
-        const upload = await uploadFile(pdf, 
-            {nomina: `${usuario.nombre}-${emision}`,
-            empresa: empresa._id })
-        
-        nomina.aws_bucket = upload.uploadParams.Bucket
-        nomina.aws_key = upload.uploadParams.Key
-        nomina.save()
+    const checks = await Check.find({
+      empleado: empleado_id,
+      fecha_entrada: { $gte: quinceDiasAtras },
+    });
 
-        const signedUrl = await getFile(upload.uploadParams.Key)
+    // verificar si aun no se ha hecho una nomina con la misma fecha de inicio y fin
 
-        const datosCorreo = {
-            correo: usuario.correo,
-            url: signedUrl,
-            usuario
-        }
-        enviarNominaTrabajador(datosCorreo)
+    let dias_laborados = checks.length;
 
-        const nominaPopulate = await Nomina.findById(nomina._id)
-            .populate({
-                path: "empleado",
-                populate: {
-                    path: "usuario"
-                }
-            })
-            .populate("plaza")
+    if (dias_laborados === 0)
+      return res
+        .status(404)
+        .json({
+          mensaje: "No hay registros de dias laborados en los últimos 15 días",
+        });
 
+    let salario_diario = Number(plaza.salario);
 
-        return res.status(200).json({ nomina: nominaPopulate, url: signedUrl })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'hubo un error'})
+    const factor_integracion = calcularFactorIntegracion(
+      dias_vacaciones,
+      dias_aginaldo
+    );
+    const salario_diario_integrado =
+      salario_diario <= 207.44
+        ? 5255.5 / 15
+        : salario_diario * factor_integracion;
+    let cuotas_obrero = calcularCuotasObrero(
+      salario_diario_integrado,
+      dias_laborados
+    );
+    let ISR = calcularISR(salario_diario, dias_laborados);
+
+    if (salario_diario <= 207.44) {
+      ISR = 0;
+      cuotas_obrero = 0;
     }
 
-    
-}   
+    const suma_deducciones = cuotas_obrero + ISR;
+    const salario_bruto = salario_diario * dias_laborados;
 
-const getNominasEmpresa = async(req, res) => {
-    const { empresa } = req
+    const nomina = await Nomina.create({
+      empleado: empleado_id,
+      empresa: empresa._id,
+      plaza: plaza._id,
+      percepciones: {
+        dias_laborados,
+        fecha_inicio: checks[0].fecha_entrada,
+        fecha_fin: checks[checks.length - 1].fecha_entrada,
+        sueldo: salario_bruto,
+        salario_diario,
+        salario_diario_integrado,
+        subsidio: calcularSubsidio(salario_diario, dias_laborados),
+        neto: salario_bruto - suma_deducciones,
+      },
+      deducciones: {
+        ISR,
+        cuotas_obrero,
+      },
+    });
 
-    try {
-        const nominas = await Nomina.find({ empresa: empresa.id })
-            .populate({
-                path: "empleado",
-                populate: {
-                    path: "usuario"
-                }
-            })
-            .populate("plaza")
+    const pdf = await generarNominaPdf({
+      nomina,
+      empleado,
+      empresa,
+      plaza,
+      usuario,
+    });
+    const emision = nomina.fecha_emision
+      .toString()
+      .split("GMT")[0]
+      .trim()
+      .replace(/ /g, "-");
+    const upload = await uploadFile(pdf, {
+      nomina: `${usuario.nombre}-${emision}`,
+      empresa: empresa._id,
+    });
 
-        if(nominas.length === 0) return res.status(404).json({ msg: 'No se encontraron nominas' })
+    nomina.aws_bucket = upload.uploadParams.Bucket;
+    nomina.aws_key = upload.uploadParams.Key;
+    nomina.save();
 
-        const urls = await getAllNominas(empresa.id)
-        
-        const nominasConUrl = nominas.map((nomina, index) => {
-            return {
-                ...nomina._doc,
-                url: urls[index]
-            }
-        })
+    const signedUrl = await getFile(upload.uploadParams.Key);
 
-        nominasConUrl.sort((a, b) => {
-            return new Date(b.fecha_emision) - new Date(a.fecha_emision)
-        })
+    const datosCorreo = {
+      correo: usuario.correo,
+      url: signedUrl,
+      usuario,
+    };
+    enviarNominaTrabajador(datosCorreo);
 
-        return res.status(200).json({ nominas: nominasConUrl })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({msg: 'hubo un error'})
-    }
-}
+    const nominaPopulate = await Nomina.findById(nomina._id)
+      .populate({
+        path: "empleado",
+        populate: {
+          path: "usuario",
+        },
+      })
+      .populate("plaza");
+
+    return res.status(200).json({ nomina: nominaPopulate, url: signedUrl });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "hubo un error" });
+  }
+};
+
+const getNominasEmpresa = async (req, res) => {
+  const { empresa } = req;
+
+  try {
+    const nominas = await Nomina.find({ empresa: empresa.id })
+      .populate({
+        path: "empleado",
+        populate: {
+          path: "usuario",
+        },
+      })
+      .populate("plaza");
+
+    if (nominas.length === 0)
+      return res.status(404).json({ msg: "No se encontraron nominas" });
+
+    const urls = await getAllNominas(empresa.id);
+
+    const nominasConUrl = nominas.map((nomina, index) => {
+      return {
+        ...nomina._doc,
+        url: urls[index],
+      };
+    });
+
+    nominasConUrl.sort((a, b) => {
+      return new Date(b.fecha_emision) - new Date(a.fecha_emision);
+    });
+
+    return res.status(200).json({ nominas: nominasConUrl });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "hubo un error" });
+  }
+};
 
 const eliminarDocumento = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    try {
-
-        const nomina = await Nomina.findById(id)
-        if(!nomina) return res.status(404).json({ msg: 'No se encontró la nomina' })
-        const result = await deleteDocument(nomina.aws_key)
-        if(result.code === 'AccessDenied') return res.status(400).json({ msg: 'No tienes permiso para eliminar este documento' })
-        await Nomina.findByIdAndDelete(id)
-        return res.status(200).json({ msg: 'Nomina eliminada' })
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json({msg: 'hubo un error'})
-    }
-}
+  try {
+    const nomina = await Nomina.findById(id);
+    if (!nomina)
+      return res.status(404).json({ msg: "No se encontró la nomina" });
+    const result = await deleteDocument(nomina.aws_key);
+    if (result.code === "AccessDenied")
+      return res
+        .status(400)
+        .json({ msg: "No tienes permiso para eliminar este documento" });
+    await Nomina.findByIdAndDelete(id);
+    return res.status(200).json({ msg: "Nomina eliminada" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ msg: "hubo un error" });
+  }
+};
 
 export default {
-    generarNomina,
-    getNominasEmpresa,
-    eliminarDocumento
-}
+  generarNomina,
+  getNominasEmpresa,
+  eliminarDocumento,
+};
--- src/helpers/pdfs/generarNominaPdf.cjs
+++ src/helpers/pdfs/generarNominaPdf.cjs
@@ -1,77 +1,121 @@
+const fs = require("fs");
+const pdfkit = require("pdfkit");
+const getStream = require("get-stream");
 
-const fs = require('fs')
-const pdfkit = require('pdfkit')
-const getStream = require('get-stream')
+const generarNominaPdf = async (body) => {
+  const { empleado, empresa, plaza, usuario, nomina: nom_body } = body;
 
+  let percepciones = [];
+  let deducciones = [];
 
-const generarNominaPdf = async(body) => {
-  const { empleado, empresa, plaza, usuario, nomina: nom_body } = body
-    const nomina = {
-        empleado: `${usuario.nombre} ${usuario.apellidos}`,
-        empresa: empresa.nombre,
-        plaza: plaza.nombre,
-        percepciones: {
-            dias_laborados: nom_body.percepciones.dias_laborados,
-            fecha_inicio: nom_body.percepciones.fecha_inicio.toLocaleDateString(),
-            fecha_fin: nom_body.percepciones.fecha_fin.toLocaleDateString(),
-            neto: nom_body.percepciones.neto.toFixed(2),
-            sueldo: nom_body.percepciones.sueldo.toFixed(2),
-            salario_diario: nom_body.percepciones.salario_diario.toFixed(2),
-            salario_diario_integrado: nom_body.percepciones.salario_diario_integrado.toFixed(2),
-            subsidio: nom_body.percepciones.subsidio.toFixed(2),
-            neto: nom_body.percepciones.neto.toFixed(2),
-        },
-        deducciones: {
-            ISR: nom_body.deducciones.ISR.toFixed(2),
-            cuotas_obrero: nom_body.deducciones.cuotas_obrero.toFixed(2),
-        },
-        fecha_emision: nom_body.fecha_emision.toLocaleDateString()
+  const nomina = {
+    empleado: `${usuario.nombre} ${usuario.apellidos}`,
+    empresa: empresa.nombre,
+    plaza: plaza.nombre,
+    neto: nom_body.percepciones.neto.toFixed(2),
+    percepciones: {
+      fecha_inicio: nom_body.percepciones.fecha_inicio.toLocaleDateString(),
+      fecha_fin: nom_body.percepciones.fecha_fin.toLocaleDateString(),
+      dias_laborados: nom_body.percepciones.dias_laborados,
+      // sueldo: nom_body.percepciones.sueldo.toFixed(2),
+      salario_diario: nom_body.percepciones.salario_diario.toFixed(2),
+      salario_diario_integrado:
+        nom_body.percepciones.salario_diario_integrado.toFixed(2),
+      subsidio: nom_body.percepciones.subsidio.toFixed(2),
+      // neto: nom_body.percepciones.neto.toFixed(2),
+    },
+    deducciones: {
+      ISR: nom_body.deducciones.ISR.toFixed(2),
+      cuotas_obrero: nom_body.deducciones.cuotas_obrero.toFixed(2),
+    },
+    fecha_emision: nom_body.fecha_emision.toLocaleDateString(),
+  };
+
+  for (const key in nomina.percepciones) {
+    if (key === "fecha_inicio" || key === "fecha_fin") {
+      percepciones.push({
+        name: key,
+        amount: nomina.percepciones[key],
+      });
+      continue;
     }
+    percepciones.push({
+      name: key,
+      amount: Number(nomina.percepciones[key]),
+    });
+  }
 
-    const doc = new pdfkit()
+  for (const key in nomina.deducciones) {
+    deducciones.push({
+      name: key,
+      amount: Number(nomina.deducciones[key]),
+    });
+  }
 
-    doc.pipe(fs.createWriteStream('output.pdf'))
-    
-    doc
-      .fontSize(25)
-      .text('Recibo de nomina', 50, 75)
-      
+  let total_percepciones = nomina.neto;
+  let total_deducciones = 0;
+  let last_y = 0;
 
-    doc
-      .fontSize(10)
-      .text(`Empleado: ${nomina.empleado}`, 50, 120)
-      .text(`Empresa: ${nomina.empresa}`, 50, 135)
-      .text(`Plaza: ${nomina.plaza}`, 50, 150)
-      .text(`Fecha: ${nomina.fecha_emision}`, 50, 165)
-      
-      .fontSize(15)
-      .text(`Percepciones`, 50, 250)
-      .underline(50, 250, 200, 15,{color: '#0000FF'})
-      .fontSize(12)
-      .text(`Sueldo: $${nomina.percepciones.sueldo}`, 50, 290)
-      .text(`Salario diario: $${nomina.percepciones.salario_diario}`, 50, 310)
-      .text(`Salario diario integrado: ${nomina.percepciones.salario_diario_integrado}`, 50, 330)
-      .text(`Subsidio: $${nomina.percepciones.subsidio}`, 50, 350)
-      .text(`Neto a recibir: $${nomina.percepciones.neto}`, 50, 370)
-      .fontSize(15)
-      .text(`Deducciones`, 350, 250).fontSize(15)
-      .underline(350, 250, 200, 15,{color: '#0000FF'})
-      .fontSize(12)
-      .text(`ISR: $${nomina.deducciones.ISR}`, 350, 290)
-      .text(`Cuotas obrero: $${nomina.deducciones.cuotas_obrero}`, 350, 310)
-      .text(`Total deducciones: $${Number(nomina.deducciones.ISR) + Number(nomina.deducciones.cuotas_obrero)}`, 350, 330)
+  const doc = new pdfkit();
 
-      .text(`Recibi de ${empresa.nombre} la cantidad de $${nomina.percepciones.neto} por concepto de sueldo correspondiente al periodo del ${nomina.percepciones.fecha_inicio} al ${nomina.percepciones.fecha_fin}`, 50, 450, {align: 'center'})
-      .fontSize(8)
-      .text(`${nomina.empleado}`, 50, 500, {align: 'center'})
-      .underline(160, 500, 255, 15,{color: '#0000', align: 'center'})
-    doc.end()
+  doc.pipe(fs.createWriteStream("output.pdf"));
 
-    const pdfBuffer = await getStream.buffer(doc)
-    
-    return pdfBuffer
+  doc.fontSize(25).text("Recibo de nomina", 25, 75);
+  doc.moveDown(0.5);
+  doc.fontSize(9).text(`Empleado: ${nomina.empleado}`);
 
-}
+  doc.moveDown(0.5);
+  doc.text(`Empresa: ${nomina.empresa}`);
+  doc.moveDown(0.5);
+  doc.text(`Plaza: ${nomina.plaza}`);
+  doc.moveDown(0.5);
+  doc.text(`Fecha: ${nomina.fecha_emision}`);
+  doc.moveDown();
+  doc.moveDown();
 
+  doc
+    .fontSize(13)
+    .text(`Percepciones`, 25, 200)
+    .underline(25, 200, 550, 15, { color: "#000000" });
+  doc.moveDown();
+  doc.fontSize(10);
+  for (let i = 0; i < percepciones.length; i++) {
+    doc.text(`${percepciones[i].name}: ${percepciones[i].amount}`);
 
-  module.exports = { generarNominaPdf }
+    doc.moveDown(0.3);
+    if (i === percepciones.length - 1) {
+      last_y = doc.y;
+    }
+  }
+  doc.fontSize(13).text(`Deducciones`, 300, 200).fontSize(15).fontSize(12);
+  doc.moveDown();
+  doc.fontSize(10);
+
+  for (let i = 0; i < deducciones.length; i++) {
+    doc.text(`${deducciones[i].name}: ${deducciones[i].amount}`);
+    total_deducciones += deducciones[i].amount;
+    doc.moveDown(0.3);
+  }
+
+  doc.text(`Total percepciones: ${total_percepciones}`, 25, last_y + 10);
+  doc.underline(25, last_y + 10, 550, 12, { color: "#CCCCCC" });
+  doc.moveDown(0.5);
+  doc
+    .text(`Total deducciones: ${total_deducciones}`, 300, last_y + 10)
+    .text(
+      `Recibi de ${empresa.nombre} la cantidad de $${nomina.percepciones.neto} por concepto de sueldo correspondiente al periodo del ${nomina.percepciones.fecha_inicio} al ${nomina.percepciones.fecha_fin}`,
+      25,
+      450,
+      { align: "center" }
+    )
+    .fontSize(8)
+    .text(`${nomina.empleado}`, 25, 500, { align: "center" })
+    .underline(160, 500, 255, 15, { color: "#0000", align: "center" });
+  doc.end();
+
+  const pdfBuffer = await getStream.buffer(doc);
+
+  return pdfBuffer;
+};
+
+module.exports = { generarNominaPdf };
