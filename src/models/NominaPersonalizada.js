a8f891b1bac8cdd6951c37df1f0187f3fc469081
Eminataren <eminataren2002@gmail.com>
2023-11-28 08:18:23 -0600

    anadiendo cositas

--- src/models/NominaPersonalizada.js
+++ src/models/NominaPersonalizada.js
@@ -1,6 +1,10 @@
 import { Schema, model } from "mongoose";
 
 export const ExtraSchema = new Schema({
+    empresa: {
+      type: Schema.Types.ObjectId,
+      ref: "Empresa"
+    },
     empleado: {
         type: Schema.Types.ObjectId,
         ref: 'Empleado',   
