6a5e70d4dcd9fbd79d83714cbfe478de8fd0617f
Eminataren <eminataren2002@gmail.com>
2024-03-17 17:52:02 -0600

    changing

--- src/controllers/pagos.js
+++ src/controllers/pagos.js
@@ -8,98 +8,99 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const endpointSecret = String(process.env.STRIPE_WEBHOOK_SECRET);
 
 const prices = async (req, res) => {
-    const prices = await stripe.prices.list();
+  const prices = await stripe.prices.list();
 
-    return res.status(200).json({prices: prices.data})
+  return res.status(200).json({ prices: prices.data })
 }
 
 
 const createCheckoutSession = async (req, res) => {
-    const { price_id } = req.params;
-    const { empresa } = req;
+  const { price_id } = req.params;
+  const { empresa } = req;
 
-    const checkoutSession = await stripe.checkout.sessions.create({
-        mode: "subscription",
-        payment_method_types: ["card"],
-        line_items: [
-            {
-                price: price_id,
-                quantity: 1,
-            }
-        ],
-        success_url: `${process.env.FRONTEND_URL}/payment-confirmation/{CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.FRONTEND_URL}/admin`,
-        customer: empresa.stripeCustomerId
-    })
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: "subscription",
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: price_id,
+        quantity: 1,
+      }
+    ],
+    success_url: `${process.env.FRONTEND_URL}/payment-confirmation/{CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.FRONTEND_URL}/admin`,
+    customer: empresa.stripeCustomerId
+  })
 
-    return res.status(200).json({ url: checkoutSession.url })
-   
+  return res.status(200).json({ url: checkoutSession.url })
+
 }
 
 
 const webhook = async (req, res) => {
 
-    const payload = req.body
-    const signature = req.headers['stripe-signature']
+  const payload = req.body
+  const signature = req.headers['stripe-signature']
+  console.log(endpointSecret)
 
-    let event;
+  let event;
 
-    try {
-        event = await stripe.webhooks.constructEvent(payload, signature, endpointSecret)
-        // console.log('Received event:', event)
-    } catch (error) {
-        console.log("Error recibido: ", error)
-        return res.status(400).send(`Webhook error: ${error.message}`)
-    }
+  try {
+    event = await stripe.webhooks.constructEvent(payload, signature, endpointSecret)
+    console.log('Received event:', event)
+  } catch (error) {
+    console.log("Error recibido: ", error)
+    return res.status(400).send(`Webhook error: ${error.message}`)
+  }
 
-    switch (event.type) {
-        case 'checkout.session.completed':
-            const session = event.data.object;
-       
-            const amount = session.amount_total / 100;
-            const usuario = await Usuario.findOne({correo: session.customer_details.email})
-            const empresa = await Empresa.findOne({creador: usuario._id})
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const session = event.data.object;
 
-            await stripe.invoiceItems.create({
-            customer: session.customer,
-            amount: session.amount_total, // El monto en centavos
-            currency: session.currency, // La moneda
-            description: 'Pago de membresía'
-        });
+      const amount = session.amount_total / 100;
+      const usuario = await Usuario.findOne({ correo: session.customer_details.email })
+      const empresa = await Empresa.findOne({ creador: usuario._id })
 
-            const invoice = await stripe.invoices.create({
-                customer: session.customer,
-                collection_method: 'send_invoice',
-                days_until_due: 30,
-            })
-            await stripe.invoices.sendInvoice(invoice.id)
-        
-            await Pago.create({
-                id_session: session.id,
-                subscription_id: session.subscription,
-                customer: session.customer,
-                id_user: empresa._id,
-                payment_status: session.payment_status,
-                amount,
-            })
-          
-            // Aquí puedes agregar el código para manejar la finalización de la sesión de Checkout
-            break;
-        case 'invoice.payment_succeeded':
+      await stripe.invoiceItems.create({
+        customer: session.customer,
+        amount: session.amount_total, // El monto en centavos
+        currency: session.currency, // La moneda
+        description: 'Pago de membresía'
+      });
 
-            break;
-        case 'invoice.payment_failed':
-            const failedPayment = event.data.object;
-            console.log('Payment failed:', failedPayment);
-            // Aquí puedes agregar el código para manejar un pago fallido
+      //const invoice = await stripe.invoices.create({
+      //  customer: session.customer,
+      //  collection_method: 'send_invoice',
+      //  days_until_due: 30,
+      //})
+      //await stripe.invoices.sendInvoice(invoice.id)
 
-        
-        default:
-           break;
-    }
+      await Pago.create({
+        id_session: session.id,
+        subscription_id: session.subscription,
+        customer: session.customer,
+        id_user: empresa._id,
+        payment_status: session.payment_status,
+        amount,
+      })
 
-    res.status(200).send()
+      // Aquí puedes agregar el código para manejar la finalización de la sesión de Checkout
+      break;
+    case 'invoice.payment_succeeded':
+
+      break;
+    case 'invoice.payment_failed':
+      const failedPayment = event.data.object;
+      console.log('Payment failed:', failedPayment);
+    // Aquí puedes agregar el código para manejar un pago fallido
+
+
+    default:
+      break;
+  }
+
+  res.status(200).send()
 }
 
 
-export default {createCheckoutSession, webhook, prices}
+export default { createCheckoutSession, webhook, prices }
